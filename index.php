0838f9cde241a93dee7a95331708bc57f99fc601
Roman <roman.dzindziura.pm.2019@lpnu.ua>
2021-09-18 16:34:52 +0300

    index

--- index.php
+++ index.php
@@ -47,42 +47,40 @@
 <th class='text-center'><span>Option</span></th>
 </tr>
 </thead>
-
 <?php
-
-    require_once 'mysql_connect.php';
-
-  $sql="SELECT * FROM connect ORDER BY id";
-  $query = $pdo->query($sql);
-  while($row = $query->fetch(PDO::FETCH_OBJ)){
-    if($row->checked == 'on'){
+require_once 'class/crud.php';
+$crud = new Crud();
+$query = "SELECT * FROM connect ORDER BY id DESC";
+$result = $crud->getData($query);
+foreach ($result as $key => $row) {
+    if($row['checked'] == 'on'){
     $chk = "<input class='cls' type='hidden' value='on'><i class='fas fa-check-circle text-success'></i></input>";
-  }else if($row->checked == 'off'){
+  }else if($row['checked'] == 'off'){
         $chk = "<input class='cls' type='hidden' value='off'><i class='fas fa-check-circle text-secondary'></i></input>";
     }
-    if($row->role == 'admin'){
+    if($row['role'] == 'admin'){
       $role = 'admin';
     }else{
       $role = 'user';
     }
     ?>
     <tbody>
-    <tr class='tr <?php echo $row->id; ?>' data-idrow = '<?php echo $row->id; ?>'>
-     <td style='width:30px'><input class='form-check-input check'  type='checkbox' value='<?php echo $row->id; ?>'></td>
-     <td class='fname'><?php echo $row->firstName ?></td>
-     <td class='lname'><?php echo $row->lastName ?></td>
+    <tr class='tr <?php echo $row['id']; ?>' data-idrow = '<?php echo $row['id']; ?>'>
+     <td style='width:30px'><input class='form-check-input check'  type='checkbox' value='<?php echo $row['id']; ?>'></td>
+     <td class='fname'><?php echo $row['firstName'] ?></td>
+     <td class='lname'><?php echo $row['lastName'] ?></td>
      <td class='checked_user'><?php echo $chk ?></td>
      <td class='role_user'><?php echo $role ?></td>
      <td>
      <ul class='list-unstyled mb-0 d-flex justify-content-center'>
      <li class='ms-2'>
 
-       <button data-id="<?php echo $row->id ?>"type='button' data-bs-target='#exampleModal' data-bs-toggle='modal' id='edit' class='btn' class='text-danger' data-toggle='tooltip' title='' data-original-title='Edit'>
+       <button data-id="<?php echo $row['id'] ?>"type='button' data-bs-target='#exampleModal' data-bs-toggle='modal' id='edit' class='btn' class='text-danger' data-toggle='tooltip' title='' data-original-title='Edit'>
          <i class='fas fa-pencil-alt'></i>
        </button>
      </li>
      <li class='ms-2'>
-     <button data-trash='<?php echo $row->id ?>' id='delete' type='button' class='btn delete text-danger'data-toggle='tooltip' title='' data-original-title='Delete' >
+     <button data-trash='<?php echo $row['id'] ?>' id='delete' type='button' class='btn delete text-danger'data-toggle='tooltip' title='' data-original-title='Delete' >
      <i class='far fa-trash-alt'></i>
      </button>
      </li>
@@ -224,6 +222,6 @@ $(document).on('click','.check',function(){
 })
 </script>
 <script src='js/script.js' charset='utf-8'></script>
+<?php require 'blocks/table1.php' ?>
 </body>
 </html>
-/* sdfsdfs */
