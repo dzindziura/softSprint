d4cd9e2720f3f2da328c0c8e8ee7302b99861eb7
Roman <roman.dzindziura.pm.2019@lpnu.ua>
2021-09-18 17:41:20 +0300

    dfsd

--- mysql_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $user = 'root';
-    $password = 'root';
-    $db = 'testing';
-    $host = 'localhost';
-
-    $dsn = 'mysql:host='.$host.';dbname='.$db;
-    $pdo = new PDO($dsn, $user, $password);
-     ?>
